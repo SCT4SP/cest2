af53461fe73db4b462d38016c48640df05a69f78
Paul Keir <paul.keir@uws.ac.uk>
2023-11-14 16:43:13 +0000

    Updated install script for constexpr headers.

--- libstdc++-v3/cest-constexpr-tests/install.sh
+++ libstdc++-v3/cest-constexpr-tests/install.sh
@@ -12,15 +12,16 @@ CPPCONFIG="$(find "$INSTALL_DIR" -type f -name c++config.h 2>/dev/null)"
 
 make -C x86_64-pc-linux-gnu/libstdc++-v3/include prefix="$INSTALL_DIR" install-data-am && make -C x86_64-pc-linux-gnu/libstdc++-v3/libsupc++ prefix="$INSTALL_DIR" install-data-am
 
+# if "c++config.h didn't exist earlier", then edit the one installed by make
+# on the lines above, to include the 2 _GLIBCXX_CEST_ ... macros
 if [ "$CPPCONFIG" == "" ]; then
-  echo File doesnT exist
-else
-  echo File EXISTS: "$CPPCONFIG"
+  CPPCONFIG="$(find "$INSTALL_DIR" -type f -name c++config.h 2>/dev/null)"
+  sed -i -e 's/#ifndef _GLIBCXX17_INLINE/\/\/ Defined if extra constexpr support is present.\n#define _GLIBCXX_CEST_VERSION 1\n\n#if _GLIBCXX_CEST_VERSION\n# ifndef _GLIBCXX_CEST_CONSTEXPR\n#  define _GLIBCXX_CEST_CONSTEXPR _GLIBCXX23_CONSTEXPR\n# endif\n#endif\n\n#ifndef _GLIBCXX17_INLINE/' "$CPPCONFIG"
 fi
 
-##### Add the following two lines before _GLIBCXX17_INLINE
-##### Remove the ##### here
-##### // Defined if extra constexpr support is present.
+##### The sed command adds the following two lines before _GLIBCXX17_INLINE
+
+# // Defined if extra constexpr support is present.
 #define _GLIBCXX_CEST_VERSION 1
 
 #if _GLIBCXX_CEST_VERSION
