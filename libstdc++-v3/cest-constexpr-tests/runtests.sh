03aeb3e429e127c5aca66ee343d9ba9b21f5d21a
Paul Keir <pkeir@outlook.com>
2023-11-23 19:46:31 +0000

    -O3 considerations.

--- libstdc++-v3/cest-constexpr-tests/runtests.sh
+++ libstdc++-v3/cest-constexpr-tests/runtests.sh
@@ -1,17 +1,20 @@
 #/bin/bash
 
+# Adding -O3 causes iostream_tests.cpp and stringstream_tests.cpp to fail due
+# to the use of a static member in ~locale(). Fix with dlsym interposition.
+
 GNUC_ROOT="$HOME/repos/SCT4SP/gcc/install"
 #GNUC_ROOT="/opt/gcc-latest"
 CLANG_ROOT="$HOME/code/ext/llvm_src/build-25-Oct-2023-Wed/llvm-project-main/install"
 #CLANG_ROOT="$HOME/repos/agozillon/clangoz_new/install"
 
-MYGCC="$GNUC_ROOT/bin/g++ -g -std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -fsanitize=address -static-libasan"
+MYGCC="$GNUC_ROOT/bin/g++ -g --std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -fsanitize=address -static-libasan"
 
 MYCLANG="$CLANG_ROOT/bin/clang++ -g -std=c++2c -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -L $GNUC_ROOT/lib64 -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address"
 
-FILES_GCC="deque_tests.cpp list_tests.cpp queue_tests.cpp forward_list_tests.cpp  map_tests.cpp set_tests.cpp iostream_tests.cpp algorithm_tests.cpp stringstream_tests.cpp cctype_tests.cpp memory_tests.cpp"
+FILES_GCC="deque_tests.cpp list_tests.cpp queue_tests.cpp forward_list_tests.cpp map_tests.cpp set_tests.cpp iostream_tests.cpp algorithm_tests.cpp stringstream_tests.cpp cctype_tests.cpp memory_tests.cpp"
 
-FILES_CLANG="deque_tests.cpp list_tests.cpp queue_tests.cpp forward_list_tests.cpp  map_tests.cpp set_tests.cpp iostream_tests.cpp algorithm_tests.cpp stringstream_tests.cpp cctype_tests.cpp memory_tests.cpp"
+FILES_CLANG="deque_tests.cpp list_tests.cpp queue_tests.cpp forward_list_tests.cpp map_tests.cpp set_tests.cpp iostream_tests.cpp algorithm_tests.cpp stringstream_tests.cpp cctype_tests.cpp memory_tests.cpp"
 
 for FILE in $FILES_GCC
 do
