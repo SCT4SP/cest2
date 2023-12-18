3eda5741c5b3b7c9cca8b7d3c1a96fb17ea7442f
Paul Keir <pkeir@outlook.com>
2023-12-18 18:26:19 +0000

    Zero parameter lambda parentheses can be omitted since P1102.

--- libstdc++-v3/cest-constexpr-tests/everything.cpp
+++ libstdc++-v3/cest-constexpr-tests/everything.cpp
@@ -22,7 +22,7 @@ constexpr bool doit()
 
   set_intersection(dq.begin(), dq.end(), l.begin(), l.end(),
                    inserter(s, s.end()));
-  function<int()> f = [&]() { return accumulate(s.begin(), s.end(), 0); };
+  function<int()> f = [&] { return accumulate(s.begin(), s.end(), 0); };
   auto x = f();
 
   map<string, int> m{{"five", 5}, {"World ", 6}};
