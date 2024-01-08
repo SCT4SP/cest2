ecb96f971964868a5b17306666a31ffbf623c85f
Paul Keir <pkeir@outlook.com>
2024-01-08 15:45:03 +0000

    Updated script for different GCC versions.

--- libstdc++-v3/cest-constexpr-tests/runtests.sh
+++ libstdc++-v3/cest-constexpr-tests/runtests.sh
@@ -4,11 +4,14 @@
 # to the use of a static member in ~locale(). Fix with dlsym interposition.
 
 GNUC_ROOT="$HOME/repos/SCT4SP/gcc/install"
-#GNUC_ROOT="/opt/gcc-latest"
+GCC_BIN=$GNUC_ROOT/bin/g++
+#GCC_BIN=/opt/gcc-latest/bin/g++ # This doesn't work ATM
 #CLANG_ROOT="$HOME/code/ext/llvm_src/build-25-Oct-2023-Wed/llvm-project-main/install"
 CLANG_ROOT=$CLANGOZ_ROOT   # Any recent (months) clang will work
 
-MYGCC="$GNUC_ROOT/bin/g++ -g -std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -fsanitize=address -static-libasan"
+# The _GLIBCXX_CEST_CONSTEXPR and _GLIBCXX_CEST_VERSION below are not required
+# when using SCT4SP/gcc/install, but
+MYGCC="$GCC_BIN -g -std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address -static-libasan"
 
 MYCLANG="$CLANG_ROOT/bin/clang++ -g -std=c++2c -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0 -I $GNUC_ROOT/constexpr-std-headers/include/c++/14.0.0/x86_64-pc-linux-gnu -L $GNUC_ROOT/lib64 -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address"
 
