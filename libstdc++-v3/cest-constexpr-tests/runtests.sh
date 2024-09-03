048a230335fd78eb9d1f7ded993492e3a5800919
Paul Keir <pkeir@outlook.com>
2024-09-03 17:06:46 +0100

    Stock Ubuntu 24.04 clang++ (18.1.3) is fine.

--- libstdc++-v3/cest-constexpr-tests/runtests.sh
+++ libstdc++-v3/cest-constexpr-tests/runtests.sh
@@ -6,14 +6,13 @@
 # Contains bin, lib64, include,constexpr-std-headers, etc.
 GNUC_ROOT="$CEST2_INCLUDE/../.."
 #GNUC_ROOT="/opt/gcc-latest" # No. Need C'est 2 binaries, for runtime, for now
-CLANG_ROOT=$CLANGOZ_ROOT   # Any recent (months) clang will work
 VER=14.0.1    # previously 14.0.0
 
 # The _GLIBCXX_CEST_CONSTEXPR and _GLIBCXX_CEST_VERSION below are not required
 # when using SCT4SP/gcc/install, but
 MYGCC="$GNUC_ROOT/bin/g++ -g -std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $CEST2_INCLUDE/c++/$VER -I $CEST2_INCLUDE/c++/$VER/x86_64-pc-linux-gnu -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address -static-libasan"
 
-MYCLANG="$CLANG_ROOT/bin/clang++ -g -std=c++2c -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $CEST2_INCLUDE/c++/$VER -I $CEST2_INCLUDE/c++/$VER/x86_64-pc-linux-gnu -L $GNUC_ROOT/lib64 -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address"
+MYCLANG="clang++ -g -std=c++26 -Winvalid-constexpr -Wl,-rpath,"$GNUC_ROOT/lib64:$LD_LIBRARY_PATH" -I $CEST2_INCLUDE/c++/$VER -I $CEST2_INCLUDE/c++/$VER/x86_64-pc-linux-gnu -L $GNUC_ROOT/lib64 -D_GLIBCXX_CEST_CONSTEXPR=constexpr -D_GLIBCXX_CEST_VERSION=1 -fsanitize=address"
 
 FILES_GCC="deque_tests.cpp list_tests.cpp queue_tests.cpp forward_list_tests.cpp map_tests.cpp set_tests.cpp iostream_tests.cpp algorithm_tests.cpp stringstream_tests.cpp cctype_tests.cpp memory_tests.cpp functional_tests.cpp policy_tests.cpp string_tests.cpp chrono_tests.cpp"
 
