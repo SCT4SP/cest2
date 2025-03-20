894880f98412269a0c3521cb600b775bda543477
Paul Keir <pkeir@outlook.com>
2025-03-20 17:38:35 +0000

    Re-added the deque tests.

--- libstdc++-v3/cest-constexpr-tests/deque_tests.cpp
+++ libstdc++-v3/cest-constexpr-tests/deque_tests.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-#if 0
 #include "tests_util.hpp"
 #include <cassert>
 #include <deque>
@@ -105,10 +103,9 @@ void deque_tests() {
   assert(push_front_dtor_test<std::deque<Bar<>>>());
   assert((deque_test5<std::deque<int>>()));
 }
-#endif
 
 int main(int argc, char *argv[])
 {
-  //deque_tests();
+  deque_tests();
   return 0;
 }
